b06a55f2546dbae8c9716d90e5387f39a706fa9a
Kwee Tang <kweetang@gmail.com>
2017-06-19 02:35:26 -0400

    Initial checkin for Log Files/Analysis

---
+++ test/log/LogEntryTest.java
@@ -0,0 +1,28 @@
+package log;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class LogEntryTest {
+	
+	@Test
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        // System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        // System.out.println(le2);
+    }
+    
+	@Test
+    public void testLogAnalyzer() {
+       LogAnalyzer LA = new LogAnalyzer();
+       
+       LA.readFile("short-test_log");
+       
+       LA.printAll();
+    }
+
+}
